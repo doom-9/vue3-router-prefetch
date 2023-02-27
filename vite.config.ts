36faaad728453fb900268356fd22953e08ace9cf
doom-0726 <110001160+doom-9@users.noreply.github.com>
2023-02-27 14:52:04 +0800

    配置打包参数

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,26 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/lib.ts'),
-      formats: ['es'],
-      fileName: 'my-lib',
+      fileName: 'lib',
+      name: 'vue3RouterPrefetch',
     },
     rollupOptions: {
       external: ['vue', 'vue-router'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          'vue': 'Vue',
+          'vue-router': 'VueRouter',
+        },
+      },
     },
   },
 })
